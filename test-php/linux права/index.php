c579a9e621fbdcbf0f6d0cde59c1bab561a707af
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2020-09-08 12:37:38 +0300

    xdebug

---
+++ test-php/linux права/index.php
@@ -0,0 +1,20 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+
+/*
+https://www.youtube.com/watch?v=wdaHKwvNRuU&list=PLg5SS_4L6LYuE4z-3BgLYGkZrs-cF4Tep&index=2
+
+
+	1)   шесттеренка справа сверху - основные настройки linux
+		postfix - навание почтового клиента в linux
+
+
+
+
+
+
+*/
+
+
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
---
+++ test.php
@@ -0,0 +1,66 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Новая страница");
+?>
+
+<?
+/*
+
+	xDebug
+
+	1) модуль xDebug  -  Это расширение , если его нет, нужно устанавливать из https://xdebug.org/ ( для своей версии php и для версии Сервера )
+
+	2) И настраиваем php.ini
+			zend_extension 					= "%sprogdir%/modules/php/%phpdriver%/ext/php_xdebug.dll"   (откуда взять модуль)
+			xdebug.remote_autostart         = on                                                        (удаленный автозапуск)
+			xdebug.remote_enable            = on                                                        (удаленный доступ)
+			xdebug.remote_handler           = "dbgp"                                                    (обработчик)
+			xdebug.remote_host              = "localhost"                                               (где)
+			xdebug.remote_port              = 9001                                                      (порт, на котором отслеживаем)
+			xdebug.remote_mode              = "req"                                                     (режим)
+			xdebug.idekey              		= "PHPSTORM"                                                (чтобы перехватить на phpstorm)
+
+	3) Настраиваем phpstorm
+		1) Settings → Languages & Frameworks → PHP
+		2) добавляем php( на php.exe ) - https://take.ms/Z7iK3
+		3) влкадка Debug
+			секция xdebug - порт такой же как выше 9001
+						   Force - 2 галочки убираем (останавливаться ли на первой строке)
+						   Can accept external connections - ставим галочку
+
+			влкадка DBGP Proxy - заполняем данными выше ( если php запускается на другом порте (не 80), нужно дописать например localhost:8080 )
+
+
+	4) устанавливаем расширение хром
+			https://chrome.google.com/webstore/detail/xdebug-helper/eadndfjplgieldjbigjakmdgkmoaaaoc
+
+	5) запускаем прослушка в phpstorm , потом в браузере.
+
+	6) Дополнительно http://xandeadx.ru/blog/raznoe/878
+
+
+	https://dev.1c-bitrix.ru/learning/course/index.php?COURSE_ID=37&LESSON_ID=3421
+	/etc/php.d
+	service httpd restart  - перезапустить apache
+
+
+	Если нужно под разные порты настраивать - https://qna.habr.com/q/403390
+ssh user@<IP-address>
+
+https://beget.com/ru/articles/xdebug
+
+ */
+?>
+<?
+echo '1';
+echo '2';
+echo '3';
+echo '4';
+
+phpinfo();
+
+
+?>
+
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
